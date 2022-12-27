a2de06d5443c29668c6c79846fae530a52b6a006
meriem <meriembenismail@insat.u-carthage.tn>
2022-12-27 08:48:43 +0100

    app routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TerrainsComponent } from './pages/terrains/terrains.component';
+import { CoachesComponent } from './pages/coaches/coaches.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TerrainsComponent,
+    CoachesComponent,
+    LoginComponent,
+    RegistrationComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
